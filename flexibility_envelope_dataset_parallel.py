86ac29a772453e7c16a4bea3b1cb62dda361630d
epaupe <epaupe@ethz.ch>
2025-12-09 10:15:47 +0100

    new buildings dataset

--- flexibility_envelope_dataset_parallel.py
+++ flexibility_envelope_dataset_parallel.py
@@ -47,17 +47,93 @@ CLIMATE_IDS = range(6)  # 0–5
 N_CORES = 6  # Number of logical CPU cores to use for parallelization use 16
 
 # Building archetypes 
-BUILDING_IDS = [
-"ep_SFH_age_0_climate_0_1241","ep_SFH_age_0_climate_0_649", "ep_SFH_age_0_climate_0_821","ep_SFH_age_0_climate_1_259", "ep_SFH_age_0_climate_1_493", "ep_SFH_age_0_climate_1_535",
-"ep_SFH_age_0_climate_2_1325", "ep_SFH_age_0_climate_2_1691", "ep_SFH_age_0_climate_2_1972",
-"ep_SFH_age_0_climate_3_955","ep_SFH_age_0_climate_3_1081", "ep_SFH_age_0_climate_3_1123","ep_SFH_age_0_climate_4_1072",
-"ep_SFH_age_0_climate_4_1688", "ep_SFH_age_0_climate_4_1709",
-"ep_SFH_age_0_climate_5_417", "ep_SFH_age_0_climate_5_758", "ep_SFH_age_0_climate_5_928",
-"ep_SFH_age_1_climate_0_42", "ep_SFH_age_1_climate_0_168", "ep_SFH_age_1_climate_0_249",
-"ep_SFH_age_1_climate_1_32","ep_SFH_age_1_climate_1_429", "ep_SFH_age_1_climate_1_458",
-"ep_SFH_age_1_climate_2_762", "ep_SFH_age_1_climate_2_852", "ep_SFH_age_1_climate_2_1161",
-"ep_SFH_age_1_climate_3_260", "ep_SFH_age_1_climate_3_451", "ep_SFH_age_1_climate_3_597"
-] 
+# BUILDING_IDS = [
+# "ep_SFH_age_0_climate_0_1241","ep_SFH_age_0_climate_0_649", "ep_SFH_age_0_climate_0_821","ep_SFH_age_0_climate_1_259", "ep_SFH_age_0_climate_1_493", "ep_SFH_age_0_climate_1_535",
+# "ep_SFH_age_0_climate_2_1325", "ep_SFH_age_0_climate_2_1691", "ep_SFH_age_0_climate_2_1972",
+# "ep_SFH_age_0_climate_3_955","ep_SFH_age_0_climate_3_1081", "ep_SFH_age_0_climate_3_1123","ep_SFH_age_0_climate_4_1072",
+# "ep_SFH_age_0_climate_4_1688", "ep_SFH_age_0_climate_4_1709",
+# "ep_SFH_age_0_climate_5_417", "ep_SFH_age_0_climate_5_758", "ep_SFH_age_0_climate_5_928",
+# "ep_SFH_age_1_climate_0_42", "ep_SFH_age_1_climate_0_168", "ep_SFH_age_1_climate_0_249",
+# "ep_SFH_age_1_climate_1_32","ep_SFH_age_1_climate_1_429", "ep_SFH_age_1_climate_1_458",
+# "ep_SFH_age_1_climate_2_762", "ep_SFH_age_1_climate_2_852", "ep_SFH_age_1_climate_2_1161",
+# "ep_SFH_age_1_climate_3_260", "ep_SFH_age_1_climate_3_451", "ep_SFH_age_1_climate_3_597"
+# ] 
+
+new_buildings = [
+    # Age 0 - Climate 0
+    "ep_SFH_age_0_climate_0_1313",
+    "ep_SFH_age_0_climate_0_1528",
+    "ep_SFH_age_0_climate_0_2571",
+    "ep_SFH_age_0_climate_0_2852",
+    "ep_SFH_age_0_climate_0_3991",
+    "ep_SFH_age_0_climate_0_4717",
+    "ep_SFH_age_0_climate_0_6144",
+
+    # Age 0 - Climate 1
+    "ep_SFH_age_0_climate_1_566",
+    "ep_SFH_age_0_climate_1_1377",
+    "ep_SFH_age_0_climate_1_1482",
+    "ep_SFH_age_0_climate_1_1515",
+    "ep_SFH_age_0_climate_1_1866",
+    "ep_SFH_age_0_climate_1_2325",
+    "ep_SFH_age_0_climate_1_2701",
+
+    # Age 0 - Climate 2
+    "ep_SFH_age_0_climate_2_65",
+    "ep_SFH_age_0_climate_2_486",
+    "ep_SFH_age_0_climate_2_545",
+    "ep_SFH_age_0_climate_2_2583",
+    "ep_SFH_age_0_climate_2_2637",
+    "ep_SFH_age_0_climate_2_3285",
+    "ep_SFH_age_0_climate_2_3494",
+
+    # Age 0 - Climate 3
+    "ep_SFH_age_0_climate_3_1303",
+    "ep_SFH_age_0_climate_3_2057",
+    "ep_SFH_age_0_climate_3_2769",
+    "ep_SFH_age_0_climate_3_3554",
+    "ep_SFH_age_0_climate_3_3711",
+    "ep_SFH_age_0_climate_3_4047",
+    "ep_SFH_age_0_climate_3_4215",
+
+    # Age 0 - Climate 4
+    "ep_SFH_age_0_climate_4_2155",
+    "ep_SFH_age_0_climate_4_3344",
+    "ep_SFH_age_0_climate_4_3470",
+    "ep_SFH_age_0_climate_4_4111",
+    "ep_SFH_age_0_climate_4_4325",
+    "ep_SFH_age_0_climate_4_4409",
+    "ep_SFH_age_0_climate_4_5146",
+
+    # Age 0 - Climate 5
+    "ep_SFH_age_0_climate_5_1289",
+    "ep_SFH_age_0_climate_5_1452",
+    "ep_SFH_age_0_climate_5_1469",
+    "ep_SFH_age_0_climate_5_1710",
+    "ep_SFH_age_0_climate_5_1991",
+    "ep_SFH_age_0_climate_5_2167",
+    "ep_SFH_age_0_climate_5_3951",
+
+    # Age 1 - Climate 0
+    "ep_SFH_age_1_climate_0_610",
+    "ep_SFH_age_1_climate_0_928",
+    "ep_SFH_age_1_climate_0_971",
+    "ep_SFH_age_1_climate_0_1102",
+    "ep_SFH_age_1_climate_0_1406",
+    "ep_SFH_age_1_climate_0_1418",
+    "ep_SFH_age_1_climate_0_1459",
+
+    # Age 1 - Climate 2
+    "ep_SFH_age_1_climate_2_1914",
+    "ep_SFH_age_1_climate_2_2059",
+    "ep_SFH_age_1_climate_2_2284",
+    "ep_SFH_age_1_climate_2_2371",
+    "ep_SFH_age_1_climate_2_2398",
+    "ep_SFH_age_1_climate_2_2719",
+    "ep_SFH_age_1_climate_2_3025",
+]
+
+
 
 # =====================================================
 # STEP 1 — OPTIMIZATION: Run MPC and retrieve UB/LB arrays
